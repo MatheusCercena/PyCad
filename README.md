8e00e6d2a2d0726e4ce3227b6f41f0e35b79f669
Matheus Cercena do Nascimento <certheus@protonmail.com>
2025-04-25 21:18:08 -0300

    grande atualizacao
    definindo lista de funcoes a fazer, definindo posicoes de LCs e armazenando em listas e permitindo desenho automatico das LCs em funcao da quantidade de Lcs

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # PyCad
- 
+
+DESENHO PRINCIPAL
+
+1 Armazenar linhas de centro para uso posterior - feito
+2 Desenhar linhas de centro - Feito
+
+3 definir seção principal do layout do projeto de acordo com as linhas de centro definidas
+4 aplicar fator de redefiniçao de posição de linhas de centro para ajustar layout de acordo com seção principal
+
+5 Dar offsets e fillets em linhas de centro pra obter trilhos
+6 desenhar linhas dos vidros e leitos individuais 
+
+7 puxar cotas dos vidros e leitos individuais 
+8 Puxar cotas ( de linhas de centro, trilho, furos, drenos) para cada linha de centro
+
+-----------------------
+MEIO-D0-PROJETO: 
+
+9 adicionar mtext com pivos e altura dos vidros, painel e vao. 
+10 adicionar lindas de sentido de abertura. 
+11 adicionar bolinhas de leito
+12 adicionar passantes/colantes
+13 adicionar angulos entre seções
+14 adicionar tabelas com vidro de guarda-corpo
+15 adicionar detalhamentos personalisados
+16 adicionar detalhamentos de corte no trilho
+17 adicionar arcos de porta
+18 adicionar medidas das molduras e quantidade de vidros, já pensando na posição das molas 
+
+------------------
+LAYOUT: 
+
+19 fazer legenda de componentes e adicionar componentes dinamicamente
+20 pegar dados da obra no ecg e completar cabeçalho
+
+21 pegar input com dados de perfis e descrição de onde vao, para adicionar nas observacoes
+22 adicionar informaçoes flutuantes para produção, instalação, transporte, usinagem, etc.
+
+-----------------
+Controle de qualidade:
+
+23 Conferir se fonte está em tamanho adequado
+24 Encaixar informações na folha
+----------------
+CADASTRO:
+
+25 Cadastrar tudo no ecg conforme projeto
--- scripts.py
+++ scripts.py
@@ -1,48 +1,67 @@
 from pyautocad import Autocad, APoint
-from math import pi, sin, cos
+from math import radians, sin, cos
 
 acad = Autocad(create_if_not_exists=True)
-print("Conectado ao AutoCAD:", acad.app.Name)
 
-lcs = []
-angs_ex = []
-angs_in = []
-prumos = []
-def conv_rad(ang: float):
-    return ang*(pi/180)
+lcs = []#linhas de centro
+angs_ex = []#angulos externos(1 esq(angs_ex[0]), 1 dir(angs_ex[1]))
+angs_in = []#angulos internos(quantidade elementos == len(lcs)-1)
+prumos = []#prumos
+
 def pedir_lcs():
-    cont = 0
+    cont = 1
     while True:
-        lc_secoes = int(input(f'Digite a linha de centro da S{cont+1}: '))
+        lc_secoes = int(input(f'Digite a linha de centro da S{cont}: '))
         lcs.append(lc_secoes)
         cont +=1
-        res = input('Deseja digitar outra linha de centro?')
-        if res == '':
+        res = input('Deseja digitar outra linha de centro? Digite enter para sim ou qualquer tecla para não:')
+        if res != '':
             break
+
 def pedir_angSecoes(qntd):
     for c in range(0, qntd-1):
         ang_sec = int(input(f'Qual o angulo entre a S{c+1} e a S{c+2}: '))
-        angs_in.append(ang_sec)
+        angs_in.append(ang_sec*-1)
+
+def definir_linhas_de_centro(lcs, angs_in):
+    '''
+    Define as posições iniciais e finais nos eixos x e y para cada uma das linhas de centro entregues no parametro lcs.\n
+    lcs: do tipo list(), com as linhas de centro a serem definidas\n
+    angs_in: do tipo list(), com os angulos internos entre cada linha de centro. Note que angs_in[0] equivale ao angulo entre lcs[0] e lcs[1]\n
+    return: retorna as posições definidas para cada linha de centro.
+    '''
+    lista_de_LCs = []
+    pos_xi = 0
+    pos_yi = 0
+    pos_xf = lcs[0]
+    pos_yf = 0
+    lc = [pos_xi, pos_yi, pos_xf, pos_yf]
+    lista_de_LCs.append(lc)
+    ang_in = 0
+
+    for c in range(1, len(lcs)):
+        pos_xi = pos_xf
+        pos_yi = pos_yf
+        pos_xf += cos(radians(angs_in[c-1] + ang_in))*lcs[c]
+        pos_yf += sin(radians(angs_in[c-1] + ang_in))*lcs[c]
+        ang_in += angs_in[c-1]
+        lc = [pos_xi, pos_yi, pos_xf, pos_yf]
+        lista_de_LCs.append(lc)
+
+    return lista_de_LCs
+
+def desenhar_linhas_de_centro(pos_lcs):
+    '''
+    Desenha as linhas de centro da instancia aberta de Autocad, de acordo com as respectivas coordenadas passadas no parametro pos_lcs.\n
+    pos_lcs: do tipo list(), com as coordenadas das linhas de centro a serem desenhadas.
+    '''
+    for lc in pos_lcs:
+        acad.model.AddLine(APoint(lc[0], lc[1]), APoint(lc[2], lc[3]))
+    
 pedir_lcs()
 pedir_angSecoes(len(lcs))
-
-lcs1_xf = lcs[0]
-lcs1_yf = 0
-lc1 = acad.model.AddLine(APoint(0, 0), APoint(lcs1_xf, lcs1_yf))
-lcs2_xf = lcs1_xf + cos(conv_rad(angs_in[0]))*lcs[1]
-lcs2_yf = lcs1_xf + sin(conv_rad(angs_in[0]))*lcs[1]
-lc2 = acad.model.AddLine(APoint(lcs1_xf, lcs1_yf), APoint(lcs2_xf, lcs2_yf))
-
-lcs3_xf = lcs2_xf + cos(conv_rad(angs_in[1]))*lcs[2]
-lcs3_yf = lcs2_yf + sin(conv_rad(angs_in[1]))*lcs[2]
-lc3 = acad.model.AddLine(APoint(lcs2_xf, lcs2_yf), APoint(lcs3_xf, lcs3_yf))
-
-lcs4_xf = lcs3_xf + cos(conv_rad(angs_in[2]))*lcs[3]
-lcs4_yf = lcs3_yf + sin(conv_rad(angs_in[2]))*lcs[3]
-lc4 = acad.model.AddLine(APoint(lcs3_xf, lcs3_yf), APoint(lcs4_xf, lcs4_yf))
-
-
-
+pos_lcs = definir_linhas_de_centro(lcs, angs_in)
+desenhar_linhas_de_centro(pos_lcs)
 
 
 # def pedir_angParedes():
@@ -55,8 +74,3 @@ lc4 = acad.model.AddLine(APoint(lcs3_xf, lcs3_yf), APoint(lcs4_xf, lcs4_yf))
 #     prumos.append(prumo_esq)
 #     prumo_dir = int(input(f'Digite o angulo da extremidade direita: '))
 #     prumos.append(prumo_dir)
-# def desenhar_linhas_de_centro():
-#     s1 = acad.model.AddLine(APoint(xi, yi), APoint(xf, yf))
-# # def rotacionar_linhas(seção1, seção2, angulo):
-# #     ponto_dir_seção1 = 
-# #     ponto_esq_seção1 = 
