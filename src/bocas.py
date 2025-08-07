3eb292aa95ce1ff46335acf12833e3ac98c37ee2
Matheus Cercena do Nascimento <certheus@protonmail.com>
2025-08-07 08:03:31 -0300

    ...

--- src/bocas.py
+++ src/bocas.py
@@ -29,7 +29,13 @@ def obter_pontos_intervalo(pontos_vidros: list[list[float]], vidro_inicial: int,
 
     return pontos_vidros[inicio:fim]
 
-def definir_pivos(sentidos, juncoes: list[list[int]], medidas_perfis_U: list[list[float]], quant_vidros):
+def definir_pivos(
+        quant_vidros: list[int],
+        sentidos: list[int, int, int, int, tuple[int]],
+        juncoes: list[list[int]],
+        medidas_perfis_U: list[list[float]],
+        pontos_vidros: list[list[float]]
+        ):
     """
     Para cada vidro, define o valor do pivô (30, 70 ou 85) conforme posição, sentido de abertura e tipo de junção.
     - sentidos: lista de tuplas/listas, cada uma com (vidro_inicial, vidro_final, giratorio, adjacente, direcao),
@@ -43,7 +49,14 @@ def definir_pivos(sentidos, juncoes: list[list[int]], medidas_perfis_U: list[lis
     for sentido in sentidos:
         giratorio = sentido[2]
         for i, lado in enumerate(vidros_mapeados):
-            # perfis_secao = medidas_perfis_U[i]
+            perfis_secao = medidas_perfis_U[i]
+            perfis_linear = []
+            inicio = 0
+            for perfil in perfis_secao:
+                ini_perfil = inicio
+                fim_perfil = inicio + perfil
+                perfis_linear.append((ini_perfil, fim_perfil))
+                inicio += perfil
             for j, vidro in enumerate(lado):
                 if vidro == giratorio:
                     if j == 0:
@@ -61,35 +74,33 @@ def definir_pivos(sentidos, juncoes: list[list[int]], medidas_perfis_U: list[lis
                             pivos.append(85)
                             break
                     else:
-                        # Verifica qual das extremidades (início ou fim) está mais próxima do vidro giratório
-                        # e puxa o pivô a partir dali (pode ser útil para casos de ajuste fino)
-                        # Aqui, vamos comparar a distância do vidro giratório até o início e até o fim do lado
-                        # dist_inicio = 
-                        # dist_fim = len(lado) - 1 - j
-                        # if dist_inicio < dist_fim:
-                        #     # Mais próximo do início
-                        #     tipo_juncao = juncoes[i][0]
-                        #     if tipo_juncao == 0:
-                        #         pivos.append(70)
-                        #     elif tipo_juncao in (1, 2) and sentido[4] == 'esquerda':
-                        #         pivos.append(85)
-                        #     else:
-                        #         pivos.append(30)
-                        # elif dist_fim < dist_inicio:
-                        #     # Mais próximo do fim
-                        #     tipo_juncao = juncoes[i][1]
-                        #     if tipo_juncao == 0:
-                        #         pivos.append(70)
-                        #     elif tipo_juncao in (1, 2) and sentido[4] == 'direita':
-                        #         pivos.append(85)
-                        #     else:
-                        #         pivos.append(30)
-                        # else:
-                        #     # Equidistante, padrão 30
-                        #     pivos.append(30)
-                        
+                        # Achar lado que começa o vidro giratório
+                        if sentido[4] == 'esquerda':
+                            ponto_ini_vidro = pontos_vidros[i][j][0]
+                        else:
+                            ponto_ini_vidro = pontos_vidros[i][j][1]
+                        # Achar em qual perfil está o vidro giratório 
+                        secao_do_giratorio = []
+                        for i, perfil in enumerate(perfis_linear):
+                            if perfil[0] <= ponto_ini_vidro <= perfil[1]:
+                                secao_do_giratorio = perfil[i]
+                                break
+                        # Verifica qual das extremidades da secao do giratorio está mais próxima do vidro
+                        dist_inicio = ponto_ini_vidro - secao_do_giratorio[0] 
+                        dist_fim = secao_do_giratorio[1] - ponto_ini_vidro
+                        if dist_inicio < dist_fim:
+                            distancia_usada = dist_inicio
+                        else:
+                            distancia_usada = dist_fim
+                        pivos.append(30 + distancia_usada)
 
-def definir_bocas(sentidos, medidas_vidros: list[float], pontos_vidros: list[list[float]], juncoes: list[list[int]], pivos):
+def definir_bocas(
+        sentidos: list[int, int, int, int, tuple[int]],
+        medidas_vidros: list[float], 
+        pontos_vidros: list[list[float]], 
+        juncoes: list[list[int]], 
+        pivos: list[int]
+        ):
     for i, sentido in enumerate(sentidos):
         vidro_ini = sentido[0]
         vidro_fim = sentido[1]
@@ -97,11 +108,9 @@ def definir_bocas(sentidos, medidas_vidros: list[float], pontos_vidros: list[lis
         medida_vidros_da_abertura = obter_medidas_intervalo(medidas_vidros, vidro_ini, vidro_fim)
         posicao_vidros_da_abertura = obter_pontos_intervalo(pontos_vidros, vidro_ini, vidro_fim)
         quant_vidros_da_abertura = contar_entre_numeros(vidro_ini, vidro_fim)
-
+        folga_vidro_giratorio = 0.0
         def definir_distancia_bocas():
             pass
         def definir_quantidade_bocas():
             pass
 
-        if sentido[4] == 'esquerda':
-               
